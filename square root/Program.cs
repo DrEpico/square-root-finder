17fe30785bda5b25f653a486a9e835e7fd1ca204
DrEpico <76069708+DrEpico@users.noreply.github.com>
2025-02-02 23:20:07 +0000

    Add some initial logic.

--- square root/Program.cs
+++ square root/Program.cs
@@ -1,2 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+
+double Y = 0; //user input
+
+double compute(double Y)
+{
+    //reject Y=0
+    double squareRoot = 0; 
+    while(Y != squareRoot)
+    {
+        squareRoot = 0.50 * (squareRoot + Y / squareRoot);
+    }
+    return Y;
+}
+
+compute(Y);
